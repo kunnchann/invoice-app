10bcc71c16e31d9fa64868cba092be8a8bd965f9
kunnchann <kunnchann9@gmail.com>
2024-01-04 13:42:39 +0630

    invoice-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, from "react";
+import React from "react";
 import Header from "./components/Header";
 import CheckoutForm from "./components/CheckoutForm";
 import RecordTable from "./components/RecordTable";
